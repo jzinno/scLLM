5d812ad888111836236898e2f6065ecb845a0858
John Zinno <johnzinno14@gmail.com>
2023-04-24 11:44:06 -0400

    add frontend image customization

---
+++ src/frontend/bg-snippet.py
@@ -0,0 +1,22 @@
+import base64
+import streamlit as st
+
+
+def sidebar_bg(side_bg):
+
+    side_bg_ext = "png"
+
+    st.markdown(
+        f"""
+      <style>
+      [data-testid="stSidebar"] > div:first-child {{
+          background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()});
+      }}
+      </style>
+      """,
+        unsafe_allow_html=True,
+    )
+
+
+side_bg = "assets\\test.png"
+sidebar_bg(side_bg)
